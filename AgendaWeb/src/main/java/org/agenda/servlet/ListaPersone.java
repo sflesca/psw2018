fdbf8a7bd5ed6982a72d8bac6280beeed7c96b57
Sergio Flesca <sergio.flesca@gmail.com>
2019-03-21 12:02:01 +0100

    xxx

--- AgendaWeb/src/main/java/org/agenda/servlet/ListaPersone.java
+++ AgendaWeb/src/main/java/org/agenda/servlet/ListaPersone.java
@@ -47,30 +47,14 @@ public class ListaPersone extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.setContentType("text/html");
-//		response.getWriter().println(
-//				"<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\r\n" + 
-//				"<html>\r\n" + 
-//				"<head>\r\n" + 
-//				"<title>Elenco Prodotti</title>\r\n" + 
-//				"</head>\r\n" + 
-//				"<body>\r\n" + 
-//				"<h1>Persone</h1>");
 		Query q = em.createNamedQuery("findAll");
 		List<Persona> lp =q.getResultList();
-//		response.getWriter().println("<ul>");
 		PersoneBean pb = new PersoneBean();
 		pb.setGroupName("ciao");
 		request.setAttribute("persone", pb);
 		for( Persona p: lp) {
 			pb.getPers().add(p);
-//			response.getWriter().println("<li>id:"+p.getId()+" nome:"+p.getNome()+"</li>");
 		}
-//		response.getWriter().println("</ul>");
-//		response.getWriter().println(
-//				"</body>\n" + 
-//				"</html>");
 		gotoPage("/Persone.jsp",request,response);
 	}
 	/**
@@ -88,3 +72,4 @@ public class ListaPersone extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 }
+
