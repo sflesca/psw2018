7647b460dfaffc7376a262101713d3e9a37ed103
sfles <sfles@DESKTOP-2E354LC>
2019-04-10 11:14:46 +0200

    InserimentiCompleti

--- AgendaWeb/src/main/java/org/agenda/ejb/CaricamentoDati.java
+++ AgendaWeb/src/main/java/org/agenda/ejb/CaricamentoDati.java
@@ -32,7 +32,24 @@ public class CaricamentoDati {
     	p1.setGruppo(g);
     	g.getPersona().add(p1);
     	em.persist(p1);
-   	
+    	Persona p2 =  new Persona();
+    	p2.setNome("Madre");
+    	p2.setGruppo(g);
+    	g.getPersona().add(p2);  	
+    	em.persist(p2);
+    	Gruppo gg = new Gruppo();
+    	gg.setNome("Amici");
+    	em.persist(gg);
+    	Persona p3 =  new Persona();
+    	p3.setNome("Amico 1");
+    	p3.setGruppo(gg);
+    	gg.getPersona().add(p3);
+    	em.persist(p3);
+    	Persona p4 =  new Persona();
+    	p4.setNome("Amico 2");
+    	p4.setGruppo(gg);
+    	gg.getPersona().add(p4);  	
+    	em.persist(p4);
     	return true;
     }
      
